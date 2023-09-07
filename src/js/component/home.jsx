0dce3ba8f0fff68d8f4324030ab21eb5ed564c30
rariasnav <137901995+rariasnav@users.noreply.github.com>
2023-09-07 13:46:48 +0000

    commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,47 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, {useState} from "react";
 
 //create your first component
 const Home = () => {
+	let [redborderColorLight, setRedBorderColor ] = useState('black');
+	let [orangeborderColorLight, setOrangeBorderColor ] = useState('black');
+	let [greenborderColorLight, setGreenBorderColor ] = useState('black');
+	function redBorderColor(){
+		if(redborderColorLight == "yellow") {
+			setRedBorderColor('black')
+		}else{
+			setRedBorderColor('yellow')
+			setOrangeBorderColor('black')
+			setGreenBorderColor('black')
+		};
+	};
+	function orangeBorderColor(){
+		if(orangeborderColorLight == "yellow") {
+			setOrangeBorderColor('black')
+		}else{
+			setOrangeBorderColor('yellow')
+			setRedBorderColor('black')
+			setGreenBorderColor('black')
+		};
+	};
+	function greenBorderColor(){
+		if(greenborderColorLight == "yellow") {
+			setGreenBorderColor('black')
+		}else{
+			setGreenBorderColor('yellow')
+			setRedBorderColor('black')
+			setOrangeBorderColor('black')
+		};
+	};
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			<div className="col bg-dark mx-auto mt-5 p-1 text-center" style={{width: "1rem", height: "3rem"}}></div>
+			<div className="col bg-dark mx-auto p-1 text-center" style={{width: "6rem", height: "13rem", borderRadius: "5%"}}>
+				<button className="d-block mx-auto text-center" style={{backgroundColor: "red", borderColor: redborderColorLight, width: "4rem", height: "4rem", borderRadius: "50%" }} onClick={redBorderColor} ></button>
+				<button className="d-block mx-auto text-center" style={{backgroundColor: "orange", borderColor: orangeborderColorLight, width: "4rem", height: "4rem", borderRadius: "50%" }} onClick={orangeBorderColor} ></button>
+				<button className="d-block mx-auto text-center" style={{backgroundColor: "green", borderColor: greenborderColorLight, width: "4rem", height: "4rem", borderRadius: "50%" }} onClick={greenBorderColor} ></button>
+			</div>
 		</div>
 	);
 };
